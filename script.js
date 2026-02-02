0d7f38d16adcda2f973958a3e195b519ecacabb8
Wesley Eduardo <wesleyeduardo.dev@gmail.com>
2026-02-02 08:15:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://devquote.com.br/api/minicurso';
+const API_BASE_URL = 'https://wesley.devquote.com.br/api/minicurso';
 //const API_BASE_URL = 'http://localhost:8090/api/minicurso';
 
 document.addEventListener('DOMContentLoaded', () => {
